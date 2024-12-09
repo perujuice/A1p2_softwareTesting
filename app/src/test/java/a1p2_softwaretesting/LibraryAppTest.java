85716e6301ed9b2e2165027a1206cc6cb5dfef79
mh226eh <mh226eh@student.lnu.se>
2024-12-09 13:16:46 +0100

    RED

--- app/src/test/java/a1p2_softwaretesting/LibraryAppTest.java
+++ app/src/test/java/a1p2_softwaretesting/LibraryAppTest.java
@@ -3,6 +3,8 @@ package a1p2_softwaretesting;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import java.io.PrintStream;
+
 import org.junit.jupiter.api.Test;
 
 
@@ -15,5 +17,16 @@ public class LibraryAppTest {
 
         sut.start(); // Call the start() method
         verify(viewMock).displayWelcomeMessage(); // Verify that the displayWelcomeMessage() method was called
+    }
+
+    @Test
+    public void testLibraryAppMain() {
+        PrintStream outStreamMock = mock(PrintStream.class); // Create a mock object of the PrintStream class
+        PrintStream originalOutStream = System.out; // Save the original output stream
+        var args = new String[0]; // Create an empty array of strings
+        System.setOut(outStreamMock); // Set the output stream to the mock object
+        LibraryApp.main(args);
+        verify(outStreamMock).println(LibraryView.WELCOME); // Verify that the welcome message was printed
+        System.setOut(originalOutStream); // Reset the output stream
     }
 }
