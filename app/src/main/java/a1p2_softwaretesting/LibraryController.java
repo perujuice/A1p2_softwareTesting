36e2db901af35fb270b693a90fc66ce15f4e9f5f
mh226eh <mh226eh@student.lnu.se>
2024-12-09 16:39:52 +0100

    REFACTOR: just added some comments

--- app/src/main/java/a1p2_softwaretesting/LibraryController.java
+++ app/src/main/java/a1p2_softwaretesting/LibraryController.java
@@ -4,11 +4,17 @@ public class LibraryController {
     private Library library;
     private LibraryView view;
 
+    /*
+     * Constructor for the LibraryController class.
+     */
     public LibraryController(Library library, LibraryView view) {
         this.library = library;
         this.view = view;
     }
 
+    /*
+     * Handle user input by calling the appropriate method based on the input.
+     */
     public void handleUserInput(String input) {
         switch (input) {
             case "1":
@@ -19,6 +25,10 @@ public class LibraryController {
         }
     }
 
+
+    /*
+     * Add a book to the library, by prompting the user for the book's title, author, ISBN, and year.
+     */
     private void addBook() {
         String title = view.promptForBookTitle();
         String author = view.promptForBookAuthor();
--- app/src/main/java/a1p2_softwaretesting/LibraryView.java
+++ app/src/main/java/a1p2_softwaretesting/LibraryView.java
@@ -22,15 +22,24 @@ public class LibraryView {
         "Please choose an option: "
     );
 
+    /*
+     * Constructor for the LibraryView class.
+     */
     public LibraryView(PrintStream outStream) {
         this.outStream = outStream;
         this.scanner = new Scanner(System.in);
     }
 
+    /*
+     * Display the welcome message.
+     */
     public void displayWelcomeMessage() {
         outStream.println(WELCOME);
     }
 
+    /*
+     * Display the console menu.
+     */
     public void displayConsoleMenu() {
         for (String option : MENU_OPTIONS) {
             if (option.equals("Please choose an option: ")) {
@@ -41,25 +50,40 @@ public class LibraryView {
         }
     }
 
+    /* 
+     * Get user input.
+     */
     public String getUserInput() {
         return scanner.nextLine();
     }
 
+    /*
+     * Prompt the user for the title of the book.
+     */
     public String promptForBookTitle() {
         outStream.print("Enter the title of the book: ");
         return scanner.nextLine();
     }
 
+    /*
+     * Prompt the user for the author of the book.
+     */
     public String promptForBookAuthor() {
         outStream.print("Enter the author of the book: ");
         return scanner.nextLine();
     }
 
+    /*
+     * Prompt the user for the ISBN of the book.
+     */
     public String promptForBookIsbn() {
         outStream.print("Enter the ISBN of the book: ");
         return scanner.nextLine();
     }
 
+    /*
+     * Prompt the user for the year of the book.
+     */
     public String promptForBookYear() {
         outStream.print("Enter the year of the book: ");
         return scanner.nextLine();
