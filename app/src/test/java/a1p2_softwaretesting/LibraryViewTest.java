33fb9f28ac3c2c83d57c3d77d51034c831067f4e
mh226eh <mh226eh@student.lnu.se>
2024-12-09 13:38:11 +0100

    RED testing the main console menu

--- app/src/test/java/a1p2_softwaretesting/LibraryViewTest.java
+++ app/src/test/java/a1p2_softwaretesting/LibraryViewTest.java
@@ -20,4 +20,25 @@ public class LibraryViewTest {
 
         verify(outStreamMock).println(LibraryView.WELCOME);
     }
+
+    @Test
+    public void shouldDisplayConsoleMenu() {
+        PrintStream outStreamMock = mock(PrintStream.class);
+
+        var sut = new LibraryView(outStreamMock);
+
+        sut.displayConsoleMenu();
+
+        verify(outStreamMock).println("Library Management System Menu:");
+        verify(outStreamMock).println("1. Add a book");
+        verify(outStreamMock).println("2. Remove a book");
+        verify(outStreamMock).println("3. Borrow a book");
+        verify(outStreamMock).println("4. Return a book");
+        verify(outStreamMock).println("5. List all books");
+        verify(outStreamMock).println("6. Add a member");
+        verify(outStreamMock).println("7. Remove a member");
+        verify(outStreamMock).println("8. List all members");
+        verify(outStreamMock).println("9. Exit");
+        verify(outStreamMock).print("Please choose an option: ");
+    }
 }
